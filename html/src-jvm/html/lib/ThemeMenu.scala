72d6d5668b29cb8d6cc6bf2026def5c9c9bc7cf4
tigidar <162025401+tigidar@users.noreply.github.com>
2026-01-05 19:21:28 +0100

    Update ThemeMenu.scala
    fix merge conflict

--- html/src-jvm/html/lib/ThemeMenu.scala
+++ html/src-jvm/html/lib/ThemeMenu.scala
@@ -2,35 +2,6 @@ package html.lib
 
 import tags.T.*
 import tagless.dsl.{given, *}
-<<<<<<< HEAD
-
-object ThemeMenu:
-  val themeFormId: "theme-menu-form" = id["theme-menu-form"]
-  val themeLabelId = id["theme-label"]
-
-import ThemeMenu.*
-// format: off
-def themeMenu =
-  ~form(idAttr := themeFormId, cls := "theme-menu grid-start")
-    >> label(
-      idAttr := themeLabelId,
-      cls := "theme-label",
-      forId := "theme-select"
-    )
-      > select(
-        idAttr := "theme-select",
-        cls := "theme-select",
-        nameAttr := "theme"
-      )
-      >> option("System", value := "")
-       > option("Light", value := "light")
-       > option("Dark", value := "dark")
-       > option("Night", value := "night")
-       > option("Day", value := "day")
-       > option("Xorian", value := "xorian")
-       > option("Inverted Day", value := "inverted-day")
-
-=======
 import dom.{Tree, Dom}
 
 object ThemeMenu:
@@ -62,4 +33,3 @@ def themeMenu =
         nameAttr := "theme"
       )
       >> themeOptions
->>>>>>> 60956bd (cleanup scalajs module, improve typesafety)
