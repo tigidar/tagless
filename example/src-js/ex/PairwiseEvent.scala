997e06d433e94b44601d9d9b2878b56de6083de8
tigidar <162025401+tigidar@users.noreply.github.com>
2026-01-05 19:18:49 +0100

    Refactor hideDiv and showDiv functions
    fix merge conflict

--- example/src-js/ex/PairwiseEvent.scala
+++ example/src-js/ex/PairwiseEvent.scala
@@ -7,28 +7,14 @@ import org.scalajs.dom.document
 import com.raquo.airstream.core.{Observer, EventStream, Signal}
 import com.raquo.airstream.ownership.{Owner, ManualOwner}
 
-<<<<<<< HEAD
-=======
 // DOM helpers using type-safe CSS classes
->>>>>>> 60956bd (cleanup scalajs module, improve typesafety)
 inline def get[A <: HTMLElement](id: String) =
   dom.document.getElementById(id).asInstanceOf[A]
 
 inline def getDiv(id: String): HTMLDivElement =
   dom.document.getElementById(id).asInstanceOf[HTMLDivElement]
 
-inline def hideDiv(id: String): Unit =
-<<<<<<< HEAD
-  getDiv(id).classList.add("is-hidden")
-
-inline def showDiv(id: String): Unit =
-  getDiv(id).classList.remove("is-hidden")
-
-final case class SwitchEvent(id: String):
-
-  def next(nextElementId: String): Option[SwitchEvent] =
-    if nextElementId == "welcome-content" then None
-=======
+inline def hideDiv(id: String): Unit = 
   getDiv(id).classList.add(isHidden.value)
 
 inline def showDiv(id: String): Unit =
@@ -153,49 +139,10 @@ object Page:
 final case class LegacySwitchEvent(id: String):
   def next(nextElementId: String): Option[LegacySwitchEvent] =
     if nextElementId == PageMap.Pages.welcomeId then None
->>>>>>> 60956bd (cleanup scalajs module, improve typesafety)
     else
       Try {
         println("Switching content from: " + id + " to: " + nextElementId)
         hideDiv(id)
         showDiv(nextElementId)
-<<<<<<< HEAD
-        SwitchEvent(nextElementId)
-      }.toOption
-
-final case class Page(pageSwitch: SwitchEvent):
-  self =>
-  val router = PageMap.router
-
-  private def updatePage(nextPage: PageType[PageState]): Unit =
-    println("Updating page to: " + nextPage)
-
-  def switchTo(anchorEvent: String): Page =
-    if router.keySet.contains(anchorEvent) then
-      val nextPage: PageType[PageState] = router(anchorEvent)
-      pageSwitch.next(nextPage.elementId) match
-        case Some(switchEvent) =>
-          updatePage(nextPage)
-          Page(switchEvent)
-        case None =>
-          val errorPage = PageMap.router(PageMap.Pages.errorId)
-          pageSwitch.next(errorPage.elementId) match
-            case Some(erroredSwitchEvent) =>
-              updatePage(errorPage)
-              Page(erroredSwitchEvent)
-            case None =>
-              // TODO: Fatal error, cannot switch to error page,
-              // probably should show an alert somehow instead of throwing
-              throw new Exception(
-                "Failed to switch error page, going to error page."
-              ) //
-    else self
-
-object Page:
-
-  val initialPage =
-    Page(SwitchEvent("welcome-content"))
-=======
         LegacySwitchEvent(nextElementId)
       }.toOption
->>>>>>> 60956bd (cleanup scalajs module, improve typesafety)
